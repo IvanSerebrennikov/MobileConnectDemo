0218692bdcef8a8446d3b52ff000663d674a1552
Ivan_S_Digital <ivan443931@gmail.com>
2019-12-06 17:51:42 +0300

    To JWT Token Refactoring

--- MobileConnectDemo/Services/MobileConnect/Models/SiAuthorizeRequestModel.cs
+++ MobileConnectDemo/Services/MobileConnect/Models/SiAuthorizeRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using MobileConnectDemo.Helpers;
+using Newtonsoft.Json;
 
 namespace MobileConnectDemo.Services.MobileConnect.Models
 {
@@ -16,47 +17,46 @@ namespace MobileConnectDemo.Services.MobileConnect.Models
 
     public class SiAuthorizeRequestObjectClaims
     {
+        [JsonProperty("response_type")]
         public string ResponseType { get; set; }
 
+        [JsonProperty("client_id")]
         public string ClientId { get; set; }
 
+        [JsonProperty("scope")]
         public string Scope { get; set; }
 
+        [JsonProperty("nonce")]
         public string Nonce { get; set; }
 
+        [JsonProperty("login_hint")]
         public string LoginHint { get; set; }
 
+        [JsonProperty("acr_values")]
         public string ArcValues { get; set; }
 
+        [JsonProperty("correlation_id")]
         public string CorrelationId { get; set; }
 
+        [JsonProperty("iss")]
         public string Iss { get; set; }
 
+        [JsonProperty("aud")]
         public string Aud { get; set; }
 
+        [JsonProperty("client_notification_token")]
         public string ClientNotificationToken { get; set; }
 
+        [JsonProperty("notification_uri")]
         public string NotificationUri { get; set; }
 
+        [JsonProperty("version")]
         public string Version { get; set; }
 
         public string ToJwtToken(string privateRsaKey)
         {
-            var payload = new Dictionary<string, object>
-            {
-                {"response_type", ResponseType},
-                {"client_id", ClientId},
-                {"scope", Scope},
-                {"nonce", Nonce},
-                {"login_hint", LoginHint},
-                {"acr_values", ArcValues},
-                {"correlation_id", CorrelationId},
-                {"iss", Iss},
-                {"aud", Aud},
-                {"client_notification_token", ClientNotificationToken},
-                {"notification_uri", NotificationUri},
-                {"version", Version}
-            };
+            var json = JsonConvert.SerializeObject(this);
+            var payload = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
 
             return payload.ToJwtTokenWithRs256(privateRsaKey);
         }
